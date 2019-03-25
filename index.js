d1fd22a56746a89fbc7b38e276d492421e0cc8d3
Peter Coles <peter@mrcoles.com>
2019-03-24 20:46:40 -0400

    give ability to request and check custom permissions

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const PERMS = { origins: ["<all_urls>"] };
 
-const main = () => {
-  const reqResultElt = document.getElementById("req-result");
-  const containsResultElt = document.getElementById("contains-result");
+const reqResultElt = document.getElementById("req-result");
+const containsResultElt = document.getElementById("contains-result");
 
+const main = () => {
   const doRequest = () =>
-    requestPermission(PERMS)
+    Promise.resolve()
+      .then(() => getRequestPerms())
+      .then(perms => chromePermissionsRequest(perms))
       .then(granted => {
         reqResultElt.innerText = granted;
       })
@@ -14,7 +16,9 @@ const main = () => {
       });
 
   const doCheckContains = () =>
-    containsPermission(PERMS)
+    Promise.resolve()
+      .then(() => getContainsPerms())
+      .then(perms => chromePermissionsContains(perms))
       .then(contains => {
         containsResultElt.innerText = contains;
       })
@@ -30,13 +34,36 @@ const main = () => {
       false
     );
 
+  getRequestPerms();
   doCheckContains();
+};
+
+const getRequestPerms = () => _getPerms("perm-req-json", "req-result-perms");
+const getContainsPerms = () =>
+  _getPerms("perm-contains-json", "con-result-perms");
+
+const _getPerms = (sourceEltId, exEltId) => {
+  let text = document.getElementById(sourceEltId).value.trim();
+  let data;
+  let error;
+  try {
+    data = JSON.parse(text);
+  } catch (e) {
+    error = e;
+    text = "invalid_json";
+  }
+  document.getElementById(exEltId).innerText = text;
+  if (error) {
+    throw error;
+  }
+  return data;
 };
 
 //
 
-const requestPermission = perm => _asPromise(chrome.permissions.request, perm);
-const containsPermission = perm =>
+const chromePermissionsRequest = perm =>
+  _asPromise(chrome.permissions.request, perm);
+const chromePermissionsContains = perm =>
   _asPromise(chrome.permissions.contains, perm);
 
 const _asPromise = (fn, arg) =>
